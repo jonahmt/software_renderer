bbf392f4ad1641802c960500ece7548175f7b824
jonah-cal <jonahmt@berkeley.edu>
2025-12-21 22:04:06 -0800

    Add texture and scene class header files.

---
+++ include/scene.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "math.h"
+#include "mesh.h"
+#include "texture.h"
+
+namespace softrender {
+
+// Represents an instance of an object in the scene.
+struct RenderObject {
+  // Whether or not this object is valid. For deleted objects, this will be
+  // false.
+  bool valid;
+  // ID of the mesh for this object.
+  uint32_t mesh_id;
+  // ID of the texture for this object.
+  uint32_t texture_id;
+  // Matrix that takes this object from local space to world space.
+  Matrix<4, 4> transform;
+};
+
+class Scene {
+ public:
+  // Adds a mesh to the scene and returns its ID.
+  uint32_t AddMesh(const Mesh& mesh);
+  // Adds a texture to the scene and returns its ID.
+  uint32_t AddTexture(const Texture& texture);
+  // Registers a render object with the specified mesh ID, texture ID, and
+  // transform matrix. Returns the ID of the registered object.
+  uint32_t RegisterObject(uint32_t mesh_id, uint32_t texture_id,
+                          const Matrix<4, 4>& transform);
+
+  // Returns a read only reference to the list of all meshes in the scene.
+  [[nodiscard]] const std::vector<Mesh>& GetMeshes() const { return meshes_; }
+  // Returns a read only reference to the list of all textures in the scene.
+  [[nodiscard]] const std::vector<Texture>& GetTextures() const {
+    return textures_;
+  }
+  // Returns a read only reference to the list of all render objects in the
+  // scene.
+  [[nodiscard]] const std::vector<RenderObject>& GetObjects() const {
+    return objects_;
+  }
+
+  // Updates the transform of the specified render object.
+  void UpdateObjectTransform(uint32_t object_id, const Matrix<4, 4>& transform);
+  // Removes the specified render object from the scene.
+  void RemoveObject(uint32_t object_id);
+
+ private:
+  // List of all meshes in the scene, indexed by mesh ID.
+  std::vector<Mesh> meshes_;
+  // List of all textures in the scene, indexed by texture ID.
+  std::vector<Texture> textures_;
+  // List of all render objects in the scene, each referencing a mesh and
+  // texture by ID.
+  std::vector<RenderObject> objects_;
+
+  // List of object IDs that are no longer in use and can be reused for a newly
+  // created object.
+  std::vector<uint32_t> free_object_ids_;
+};
+
+}  // namespace softrender
---
+++ include/texture.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+namespace softrender {
+
+class Texture {
+ public:
+  // Gets the pixel color at the specified coordinates (x, y) as an RGBA
+  // uint32_t.
+  [[nodiscard]] uint32_t GetPixel(int x, int y) const;
+  // Sets the pixel color at the specified coordinates (x, y) to the given RGBA
+  // color.
+  void SetPixel(int x, int y, uint32_t color);
+
+  // Returns the width of the texture in pixels.
+  [[nodiscard]] int GetWidth() const { return width_; }
+  // Returns the height of the texture in pixels.
+  [[nodiscard]] int GetHeight() const { return height_; }
+
+ private:
+  // Width of the texture in pixels.
+  int width_;
+  // Height of the texture in pixels.
+  int height_;
+  // Data stored as RGBA pixels in row major order.
+  std::vector<uint32_t> data_;
+};
+
+}  // namespace softrender
