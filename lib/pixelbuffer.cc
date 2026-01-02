092ea0f0609eedd15b4bd60d0318d16a4013801a
jonah-cal <jonahmt@berkeley.edu>
2026-01-01 22:59:30 -0800

    Implement Pixelbuffer class

--- lib/pixelbuffer.cc
+++ lib/pixelbuffer.cc
@@ -1,11 +1,21 @@
 #include "pixelbuffer.h"
 
+#include <cassert>
+
 namespace softrender {
 
 Pixelbuffer::Pixelbuffer(int width, int height)
     : width(width), height(height), data(width * height, 0) {}
 
-void Pixelbuffer::SetPixel(int x, int y, uint32_t color) {}
+void Pixelbuffer::SetPixel(int x, int y, uint32_t color) {
+  assert(x >= 0 && x < width);
+  assert(y >= 0 && y < height);
+  data[y * width + x] = color;
+}
 
-uint32_t Pixelbuffer::GetPixel(int x, int y) const { return 0; }
+uint32_t Pixelbuffer::GetPixel(int x, int y) const {
+  assert(x >= 0 && x < width);
+  assert(y >= 0 && y < height);
+  return data[y * width + x];
+}
 }  // namespace softrender
